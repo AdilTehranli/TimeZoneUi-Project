d56e550f7a34f85baf4d4cff8983f294a79eb16c
Adil Tehranli <adiljt@code.edu.az>
2023-09-28 21:31:07 +0400

    Updated footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./layouts/footer/Footer";
 import Home from "./pages/home/Home";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
    <Navbar/>
    <Home/>
+   <Footer/>
     </div>
   );
 }
---
+++ src/layouts/footer/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "../../layouts/footer/Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="footer__logo col-3">
+            <img
+              src="	https://themewagon.github.io/timezone/assets/img/logo/logo2_footer.png"
+              alt=""
+            />
+            <p>
+              Asorem ipsum adipolor sdit amet, consectetur adipisicing elitcf
+              sed do eiusmod tem.
+            </p>
+          </div>
+          <div className="footer__orderlists col-3">
+          <ul className="footer__orderlists__ul">
+              Quick Links
+              <li>About</li>
+              <li>Offers & Discounts</li>
+              <li>Get Coupon</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="footer__orderlists col-3">
+            <ul className="footer__orderlists__ul"> 
+          
+
+            New Products
+            
+              <li>Woman Cloth</li>
+              <li>Fashion Accessories</li>
+              <li>Man Accessories</li>
+              <li>Rubber made Toys</li>
+            </ul>
+          </div>
+          <div className="footer__orderlists col-3">
+          <ul className="footer__orderlists__ul">
+            Support
+              <li>Frequently Asked Questions</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+              <li>Report a Payment Issue</li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div className="footer__title d-flex">
+            <p>Copyright ©2023 All rights reserved | This template is made with  by Tehranli</p>
+        <div className="footer__icons d-flex">
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-behance"></i>
+        <i class="fa-solid fa-globe"></i>
+        </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
