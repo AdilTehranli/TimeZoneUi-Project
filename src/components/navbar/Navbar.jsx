e30df5c64e0de6d43597f941380f6a4c5a4c479f
Adil Tehranli <adiljt@code.edu.az>
2023-09-28 09:57:02 +0400

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../navbar/Navbar.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar__all">
+            <div className="navbar__logo">
+              <img
+                src="https://themewagon.github.io/timezone/assets/img/logo/logo.png"
+                alt=""
+              />
+            </div>
+            <div className="navbar__orderlist">
+              <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="navbar__icons">
+              <i className="fa-solid fa-magnifying-glass"></i>
+              <i className="fa-regular fa-user"></i>
+              <i className="fa-solid fa-basket-shopping"></i>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
