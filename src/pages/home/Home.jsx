ae70d28a471f795a39503ffbb5d2f5a82a11c344
Adil Tehranli <adiljt@code.edu.az>
2023-09-28 16:40:17 +0400

    Updated video

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "../home/Home.scss";
 import { Carousel } from "antd";
 import Products from "../../components/products/Products";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
+import video from "../../assets/videos/Rolex Glidelock – Take it up a notch.mp4";
 AOS.init();
 const contentStyle = {};
 const Home = () => {
@@ -20,17 +22,15 @@ const Home = () => {
               <h3 style={contentStyle}>
                 <div className="slider__all d-flex">
                   <div className="slider__title">
-                  <div data-aos="fade-right">
-
-
-                    <h1>Select Your New Perfect Style</h1>
-                    <p>
-                      Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                      laboris nisi ut aliquip ex ea <br /> commodo consequat is
-                      aute irure.
-                    </p>
-                    <button>SHOP NOW</button>
-                  </div>
+                    <div data-aos="fade-right">
+                      <h1>Select Your New Perfect Style</h1>
+                      <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea <br /> commodo
+                        consequat is aute irure.
+                      </p>
+                      <button>SHOP NOW</button>
+                    </div>
                   </div>
                   <div className="slider__image">
                     <img
@@ -77,37 +77,34 @@ const Home = () => {
             <h2>New Arrivals</h2>
           </div>
           <div className="arrivals__products d-flex">
-
             <div className="arrivals__product col-4">
-          <div data-aos="zoom-in-up">
-              <img
-                src="	https://themewagon.github.io/timezone/assets/img/gallery/new_product1.png"
-                alt=""
+              <div data-aos="zoom-in-up">
+                <img
+                  src="	https://themewagon.github.io/timezone/assets/img/gallery/new_product1.png"
+                  alt=""
                 />
-                </div>
-                
+              </div>
+
               <h3>Thermo Ball Etip Gloves</h3>
               <p>$17,90</p>
             </div>
             <div className="arrivals__product col-4">
-            <div data-aos="zoom-in-up">
-
-              <img
-                src="		https://themewagon.github.io/timezone/assets/img/gallery/new_product2.png"
-                alt=""
+              <div data-aos="zoom-in-up">
+                <img
+                  src="		https://themewagon.github.io/timezone/assets/img/gallery/new_product2.png"
+                  alt=""
                 />
-                </div>
+              </div>
               <h3>Thermo Ball Etip Gloves</h3>
               <p>$17,90</p>
             </div>
             <div className="arrivals__product col-4">
-            <div data-aos="zoom-in-up">
-
-              <img
-                src="		https://themewagon.github.io/timezone/assets/img/gallery/new_product3.png"
-                alt=""
+              <div data-aos="zoom-in-up">
+                <img
+                  src="		https://themewagon.github.io/timezone/assets/img/gallery/new_product3.png"
+                  alt=""
                 />
-                </div>
+              </div>
               <h3>Thermo Ball Etip Gloves</h3>
               <p>$17,90</p>
             </div>
@@ -116,22 +113,46 @@ const Home = () => {
       </div>
       <div className="gallery d-flex">
         <div className="gallery__image__left col-6">
-        <img src="https://qph.cf2.quoracdn.net/main-qimg-cbb5dff8c6b239a3caf0c2db6f23e326-lq" alt="" />
+          <img
+            src="https://qph.cf2.quoracdn.net/main-qimg-cbb5dff8c6b239a3caf0c2db6f23e326-lq"
+            alt=""
+          />
         </div>
         <div className="gallery__image__right col-6">
-            <img className="gallery__image__right__middle" src="https://assets.phillips.com/image/upload/t_Website_LotDetailMainImage/v1/auctions/CH080220/177_001.jpg" alt="" />
-            <img className="gallery__image__right__end"  src="https://amz.luxewatches.co.uk/app/uploads/2021/09/15091140/coral-dial.jpg" alt="" />
+          <img
+            className="gallery__image__right__middle"
+            src="https://assets.phillips.com/image/upload/t_Website_LotDetailMainImage/v1/auctions/CH080220/177_001.jpg"
+            alt=""
+          />
+          <img
+            className="gallery__image__right__end"
+            src="https://amz.luxewatches.co.uk/app/uploads/2021/09/15091140/coral-dial.jpg"
+            alt=""
+          />
         </div>
       </div>
 
       <div className="items">
         <div className="container">
-            <div className="item__title">
-                <h3>Popular Items</h3>
-                <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna <br /> aliqua. Quis ipsum suspendisse ultrices gravida.</p>
-            </div>
-            <Products/>
+          <div className="items__title">
+            <h3>Popular Items</h3>
+            <p>
+              Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna <br /> aliqua. Quis ipsum suspendisse
+              ultrices gravida.
+            </p>
+          </div>
+          <Products />
+          <div className="items__btn">
+            <button>VIEW MORE PRODUCTS</button>
+          </div>
         </div>
+      </div>
+
+      <div className="video">
+        <Video autoPlay loop>
+          <source src={video} type="video/webm" />
+        </Video>
       </div>
     </div>
   );
