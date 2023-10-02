81ba8b30de3b09dba5828564c71c842185836a2c
Adil Tehranli <adiljt@code.edu.az>
2023-10-02 12:44:56 +0400

    Updated blog-search

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,45 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../blog/Blog.scss';
 import Slider from '../../components/slider/Slider';
 import { FaComments } from 'react-icons/fa';
 import { AiOutlineSearch } from 'react-icons/ai';
+import axios from 'axios';
+
 const Blog = () => {
-  return (
-    <div>
-        <Slider title={"Blog"}/>
-        <div className="blog">
-            <div className="container">
-                <div className="row">
+    const [item, setItem] = useState([]);
+    const [q, setQ] = useState("");
+    const [filteredItems, setFilteredItems] = useState([]);
 
-                <div className="blog__main col-8">
-                    <div className="blog__main__image">
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+            .then(res => {
+                setItem(res.data);
+                setFilteredItems(res.data);  
+            })
+            .catch(err => console.error(err));
+    }, []);
 
-                    <img src="https://themewagon.github.io/timezone/assets/img/blog/single_blog_1.png" alt="" />
-                    <div className="blog__main__position">
-                        <p>15 Jan</p>
-                    </div>
-                    </div>
-                    <div className="blog__main__texts">
+    const handleSearch = () => {
+        const filteredItems = item.filter(product =>
+            product.title.toLowerCase().includes(q.toLowerCase())
+        );
+        setFilteredItems(filteredItems);
+    };
 
-                    <h4>Google inks pact for new 35-storey office</h4>
-                    <p>That dominion stars lights dominion divide years for fourth have don't stars is that he earth it  <br /> first without heaven in place seed it second morning saying.</p>
-                    <p className='blog__icon'> <FaComments/> 03 Comments</p>
+    return (
+        <div>
+            <Slider title={"Blog"} />
+            <div className="blog">
+                <div className="container">
+                    <div className="row">
+                        <div className="blog__main col-8">
+                            {filteredItems.length === 0 ? (
+                                <div className="not-available">
+                                    <p>Not Available</p>
+                                </div>
+                            ) : (
+                                filteredItems.map((product, index) => (
+                                    <div key={index} className="blog__main__item">
+                                        <div className="blog__main__image">
+                                            <img src={product.image} alt="" />
+                                            <div className="blog__main__position">
+                                                <p>15 Jan</p>
+                                            </div>
+                                        </div>
+                                        <div className="blog__main__texts">
+                                            <h4>{product.title}</h4>
+                                            <p>
+                                                {product.description}
+                                            </p>
+                                            <p className="blog__icon">
+                                                <FaComments /> 03 Comments
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))
+                            )}
+                        </div>
+
+                        <div className="blog__funktions col-4">
+                            <div className="blog__funktions__inp">
+                                <input
+                                    className='form-control mt-3'
+                                    type="text"
+                                    placeholder='Search'
+                                    value={q}
+                                    onChange={(e) => setQ(e.target.value)}
+                                    name='search-form'
+                                />
+                                <div className="blog__funktions__icon">
+                                    <AiOutlineSearch className='blog__funktions__search' />
+                                </div>
+                            </div>
+                            <button onClick={handleSearch}>SEARCH</button>
+                        </div>
                     </div>
-                </div>
-                <div className="blog__funktions col-4">
-                    <div className="blog__funktions__inp">
-                    <input className='form-control mt-3' type="text" placeholder='Search'  />
-                    <div className="blog__funktions__icon">
-                        <AiOutlineSearch className='blog__funktions__search' />
-                    </div>
-                    </div>
-                    <button>SEARCH</button>
-                </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Blog
+export default Blog;
