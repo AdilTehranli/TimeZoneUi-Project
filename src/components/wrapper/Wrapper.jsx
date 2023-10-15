c9edf06a4112d581891f664a73fab72cc0abe295
Adil Tehranli <adiljt@code.edu.az>
2023-10-15 11:13:31 +0400

    Updated Wrapper axios

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../wrapper/Wrapper.scss";
+import axios from "axios";
 
 const Wrapper = () => {
-  return (
+ const [wrapper, setWrapper] = useState([]);
+
+ useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://localhost:7027/api/Wrappers');
+      setWrapper(result.data);
+    };
+
+    fetchData();
+ }, []);
+
+ return (
     <div>
       <div className="container">
         <div className="wrapper">
           <div className="row">
-            <div className="wrapper__texts col-4">
-              <i class="fa-solid fa-truck-fast"></i>
-              <h4>Free Shipping Method</h4>
-              <p>aorem ixpsacdolor sit ameasecur <br /> adipisicing elitsf edasd.</p>
-            </div>
-
-            <div className="wrapper__texts col-4">
-            <i class="fa-solid fa-unlock"></i>
-              <h4>Free Shipping Method</h4>
-              <p>aorem ixpsacdolor sit ameasecur <br /> adipisicing elitsf edasd.</p>
-            </div>
-
-            <div className="wrapper__texts col-4">
-            <i class="fa-solid fa-arrows-rotate"></i>
-              <h4>Free Shipping Method</h4>
-              <p>aorem ixpsacdolor sit ameasecur <br /> adipisicing elitsf edasd.</p>
-            </div>
+            {
+              wrapper.map((item, index) => (
+                <div key={index} className="wrapper__texts col-4">
+                 <i class={item.icon}></i>
+                 <h4>{item.title}</h4>
+                 <p>{item.description}</p>
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
     </div>
-  );
+ );
 };
 
 export default Wrapper;
