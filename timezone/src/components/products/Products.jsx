1e3c60ff578561bdd09dc6ffcbddee8264fc4be2
Adil Tehranli <adiljt@code.edu.az>
2023-10-18 15:06:29 +0400

    Updated Add admin

--- timezone/src/components/products/Products.jsx
+++ timezone/src/components/products/Products.jsx
@@ -9,12 +9,12 @@ import { addlike } from '../../redux/slice/LikeSlice';
 
 const Products = ({ items }) => {
   const [likedProducts, setLikedProducts] = useState([]);
+  const [item, setItems] = useState([]); 
   const [cartProducts, setCartProducts] = useState([]);
-  const dispatch = useDispatch();
-
-  const handleAddToCart = (product) => {
+  const dispatch=useDispatch()
+  const handleAddToCart = (product) => {  
     const existingProduct = cartProducts.find((p) => p.id === product.id);
-
+  
     if (existingProduct) {
       const updatedCart = cartProducts.map((p) =>
         p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
@@ -23,19 +23,20 @@ const Products = ({ items }) => {
     } else {
       dispatch(addToCart({ ...product, quantity: 1 }));
     }
-  };
+  
 
+  };
+  
   const handleLikeProduct = (product) => {
     dispatch(addlike(product));
     toast.success('Product added to wishlist');
   };
-
   useEffect(() => {
     if (items && items.length > 0) {
-      setLikedProducts(items);
+      setItems(items); 
     }
   }, [items]);
-
+  
   if (!items) {
     return <div>Loading...</div>;
   }
@@ -43,35 +44,34 @@ const Products = ({ items }) => {
   if (items.length === 0) {
     return <div>No items available</div>;
   }
+  if (!item || item.length === 0) {
+    return <div>No items available</div>;
+  }
 
   return (
     <div className="products">
       <div className="container">
         <div className="row">
-          {items.map((product, index) => (
+          {item.map((product, index) => (
             <div key={index} className="col-4 products__item">
               <div className="products__item__container">
                 <div className="products__item__image">
                   <img src={`https://localhost:7027//${product.productImage}`} alt="" />
                   <div className="products__item__image__icon">
-                    <i className="fa-regular fa-heart" onClick={() => handleLikeProduct(product)}></i>
+                  <i className="fa-regular fa-heart" onClick={() => handleLikeProduct(product)}></i>
                   </div>
                   <div className="products__item__image__text">
-                    <p
-                      onClick={() => {
-                        handleAddToCart(product);
-                        toast.success('Added to the cart');
-                      }}
-                    >
-                      Add To Cart
-                    </p>
+                  <p onClick={() => {
+  handleAddToCart(product);toast.success('Added to the card');
+}}>Add To Cart</p>
+
                   </div>
                 </div>
                 <div className="products__item__title">
                   <Link className="title__link" to={`/productdetail/${product.id}`}>
                     <h3>{product.title}</h3>
                   </Link>
-                  <p className="category">{product.category}</p>
+                  <p className='category'>{product.category}</p>
                   <p>{product.brand}</p>
                   <p>${product.price}</p>
                 </div>
@@ -80,18 +80,17 @@ const Products = ({ items }) => {
           ))}
         </div>
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={1500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
+<ToastContainer
+position="top-right"
+autoClose={1500}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"/>
     </div>
   );
 };
