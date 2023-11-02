64c9ef927a1642b7d866789c603a006a967803f7
Adil Tehranli <adiljt@code.edu.az>
2023-11-02 13:31:59 +0400

    Updated About page responsive

--- timezone/src/pages/about/About.jsx
+++ timezone/src/pages/about/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Slider from "../../components/slider/Slider";
-import '../about/About.scss';
+import "../about/About.scss";
 import { DefaultPlayer as Video } from "react-html5video";
 import "react-html5video/dist/styles.css";
 import video from "../../assets/videos/Rolex Glidelock – Take it up a notch.mp4";
@@ -8,43 +8,36 @@ import Wrapper from "../../components/wrapper/Wrapper";
 import axios from "axios";
 
 const About = () => {
-  const [about,setAbout]=useState([]);
-  useEffect(()=>{
-    axios.get('https://localhost:7027/api/Abouts/GetAbout')
-    .then(res=>{
-    setAbout(res.data);
-    })
-  },[])
+  const [about, setAbout] = useState([]);
+  useEffect(() => {
+    axios.get("https://localhost:7027/api/Abouts/GetAbout").then((res) => {
+      setAbout(res.data);
+    });
+  }, []);
   return (
     <div>
       <Slider title="About Us" />
 
       <div className="about">
         <div className="container">
-          {
-
-            about.map((item,index)=>(
-
-          <div className="about__title">
-            <h3>
-              <span></span> {item.title}
-            </h3>
-            <p className="about__desc">
-            {item.description}
-            </p>
+          <div className="row">
+            {about.map((item, index) => (
+              <div key={index} className="about__title col-xl-7 col-lg-10 col-md-12 col-sm-10 ">
+                <h3>
+                  <span></span> {item.title}
+                </h3>
+                <p className="about__desc">{item.description}</p>
+              </div>
+            ))}
           </div>
-            ))
-          }
-        
-
         </div>
       </div>
       <div className="video">
-        <Video >
+        <Video>
           <source src={video} type="video/webm" />
         </Video>
       </div>
-      <Wrapper/>
+      <Wrapper />
     </div>
   );
 };
